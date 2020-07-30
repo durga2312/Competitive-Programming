fae7973c10ff02ad01ce6e12eb957c69cef4f108
durga2312 <68999746+durga2312@users.noreply.github.com>
2020-07-31 01:26:42 +0530

    abc160A ; cakewalk

---
+++ ATCODER/coffe_like.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	string s;
+	cin>>s;
+	if((s[2]==s[3])&&(s[4]==s[5]))
+	{
+		cout<<"Yes"<<"\n";
+	}
+	else
+	{
+		cout<<"No"<<"\n";
+	}
+	return 0;
+}
