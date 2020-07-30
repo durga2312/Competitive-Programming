b2b0e14584fb18696c41dffcf23b97737ccff0b6
durga2312 <68999746+durga2312@users.noreply.github.com>
2020-07-31 01:19:54 +0530

    abc161A ; cakewalk

---
+++ ATCODER/swap.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll a;
+	ll b;
+	ll c;
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	ll aa=0;
+	ll bb=0;
+	ll cc=0;
+	ll aaa=0;
+	bb=a;
+	aa=b;
+	cc=aa;
+	aaa=c;
+	cout<<aaa<<" "<<bb<<" "<<cc<<"\n";
+	return 0;
+}
