dc021cb3352104649f41bc896f2c799fa0f31e04
durga2312 <68999746+durga2312@users.noreply.github.com>
2020-08-10 14:54:21 +0530

    abc152A ; cakewalk

---
+++ ATCODER/AC_WC.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll n;
+	cin>>n;
+	ll m;
+	cin>>m;
+	if(n==m)
+	{
+		cout<<"Yes"<<"\n";
+	}
+	else
+	{
+		cout<<"No"<<"\n";
+	}
+	return 0;
+	
+}
