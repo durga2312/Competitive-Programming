d16f2107e00466c4ca47bbf9813813c239efde33
durga2312 <68999746+durga2312@users.noreply.github.com>
2020-08-12 01:20:52 +0530

    abc145A ; cakewalk

---
+++ ATCODER/square.cpp
@@ -0,0 +1,13 @@
+//    https://atcoder.jp/contests/abc145/tasks/abc145_a
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll r;
+	cin>>r;
+	cout<<(r*r)<<"\n";
+	return 0;
+}
