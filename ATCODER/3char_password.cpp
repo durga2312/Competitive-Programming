280a2f6a20b92c9f91c6ee457af30ab640e781c0
durga2312 <68999746+durga2312@users.noreply.github.com>
2020-08-13 09:37:32 +0530

    abc140A ; cakewalk

---
+++ ATCODER/3char_password.cpp
@@ -0,0 +1,13 @@
+//https://atcoder.jp/contests/abc140/tasks/abc140_a
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll n;
+	cin>>n;
+	cout<<(n*n*n)<<"\n";
+	return 0;
+}
