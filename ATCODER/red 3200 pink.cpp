b406731949e52b24455dbd9dfdce2dcd2773dcbb
durga2312 <68999746+durga2312@users.noreply.github.com>
2020-09-02 00:15:47 +0530

    abc138A ; cakewalk

---
+++ ATCODER/red 3200 pink.cpp
@@ -0,0 +1,22 @@
+//https://atcoder.jp/contests/abc138/tasks/abc138_a
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll a;
+	cin>>a;
+	string s;
+	cin>>s;
+	if(a>=3200)
+	{
+		cout<<s<<"\n";
+	}
+	else
+	{
+		cout<<"red"<<"\n";
+	}
+	return 0;
+}
