f89006f5a93ec35200327ca5352046717968013a
durga2312 <68999746+durga2312@users.noreply.github.com>
2020-09-02 00:43:48 +0530

    abc134A ; cake cakewalk

---
+++ ATCODER/dodgecon.cpp
@@ -0,0 +1,16 @@
+//https://atcoder.jp/contests/abc134/tasks/abc134_a
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll r;
+	cin>>r;
+	ll area;
+	area=3*r*r;
+	cout<<area<<"\n";
+	
+	return 0;
+}
