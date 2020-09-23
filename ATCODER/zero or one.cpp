1e16a3e547deeb998808cf8499cf8ec6fad74743
durga2312 <68999746+durga2312@users.noreply.github.com>
2020-09-23 15:32:40 +0530

    abc178A ; cakewalk

---
+++ ATCODER/zero or one.cpp
@@ -0,0 +1,20 @@
+//https://atcoder.jp/contests/abc178/tasks/abc178_a
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll x;
+	cin>>x;
+	if(x==0)
+	{
+		cout<<1<<"\n";
+	}
+	else
+	{
+		cout<<0<<"\n";
+	}
+	return 0;
+}
